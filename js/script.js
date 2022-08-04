26e24322fdd779e564b5d0ab4beb5cd1d2372235
andrew_v <andrewvogel@protonmail.com>
2022-08-04 11:40:16 -0400

    Created dynamic search function

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const username = "AndyV83";
 const repoList = document.querySelector(".repo-list");
 const allRepos = document.querySelector(".repos");
 const repoData = document.querySelector(".repo-data");
+const viewReposButton = document.querySelector(".view-repos");
+const filterInput = document.querySelector(".filter-repos");
 
 const githubUserInfo = async function () {
     const userInfo =await fetch(`https://api.github.com/users/${username}`);
@@ -27,7 +29,7 @@ const displayUserInfo = function (data) {
     </div>
   `;
   overview.append(div);
-  gitRepos();
+  gitRepos(username);
 };
 
 const gitRepos = async function () {
@@ -83,3 +85,25 @@ const displayRepoInfo = function (repoInfo, languages) {
     `;
     repoData.append(div);
 };
+
+viewReposButton.addEventListener("click", function (e) {
+    allRepos.classList.remove("hide");
+    repoData.classList.add("hide");
+    viewReposButton.classList.add("hide");
+});
+
+//Dynamic search
+filterInput.addEventListener("input", function (e) {
+    const searchText = e.target.value;
+    const repos = document.querySelectorAll(".repo");
+    const searchLowerText = searchText.toLowerCase();
+
+    for (const repo of repos) {
+        const repoLowerText = repo.innerText.toLowerCase();
+        if (repoLowerText.inclues(searchLowerText)) {
+            repo.classList.remove("hide");           
+        } else {
+            repo.classList.add("hide");
+        }
+    }
+});
